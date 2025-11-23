a55771c114e247c5560b00e84934d7e0c2c55446
Larissa de Oliveira <69212760+lariadeoliveira@users.noreply.github.com>
2025-11-23 00:38:01 -0300

    Add hotel reservation example in Ex1_HotelReservation

---
+++ Ex1_HotelReservation.java
@@ -0,0 +1,13 @@
+public class Ex1_HotelReservation {
+public static void main(String[] args) {
+int numeroQuarto = 305;
+double valorDiaria = 259.90;
+String nomeHospede = "Ana Silva";
+
+
+System.out.println("Reserva de quarto:");
+System.out.println("Número do quarto: " + numeroQuarto);
+System.out.println("Valor da diária: R$ " + valorDiaria);
+System.out.println("Nome do hóspede: " + nomeHospede);
+}
+}
